80bcb050d94baf55a979089daf05d8633cd72dc7
Woomy <WoomyManmenmi@gmail.com>
2016-06-09 06:48:28 -0400

    Escape N-ZAP '83

--- splatoon.js
+++ splatoon.js
@@ -105,7 +105,7 @@ var splatoon = {
 		{name:'Refurbished Mini Splatling', subWeapon:'Burst Bomb', specialWeapon:'Bomb Rush'},
 		{name:'Sploosh-o-matic 7', subWeapon:'Splat Bomb', specialWeapon:'Inkzooka'},
 		{name:'Aerospray PG', subWeapon:'Burst Bomb', specialWeapon:'Kraken'},
-		{name:'N-ZAP '83', subWeapon:'Point Sensor', specialWeapon:'Kraken'},
+		{name:'N-ZAP \'83', subWeapon:'Point Sensor', specialWeapon:'Kraken'},
 		{name:'Grim Range Blaster', subWeapon:'Burst Bomb', specialWeapon:'Killer Wail'},
 		{name:'Cherry H-3 Nozzlenose', subWeapon:'Splash Wall', specialWeapon:'Bubbler'},
 		{name:'CoroCoro Splat Roller', subWeapon:'Splash Wall', specialWeapon:'Inkzooka'},
